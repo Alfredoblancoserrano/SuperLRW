c0acfa88a4e2b82adc55b6185a057362f2c02952
Alfredoblancoserrano <84923360+Alfredoblancoserrano@users.noreply.github.com>
2021-06-30 23:20:20 -0300

    Delete Compilar

--- Scripts/MSD Scripts/Markov Chain approach/General adjacency matrices/complilar_caminante_normal.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-#===========================================================================
-gfortran -O3 -funroll-loops module_caminante_normal.f90 main_caminante_normal.f90 -o TH1225.out
